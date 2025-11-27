99117befbfd6dfdbb6f44532d1a6d6c3474fb7c9
Vann-ui <H1101241066@student.untan.ac.id>
2025-11-27 19:22:50 +0700

    API classcontencontroller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClassContentController;
 
 // --- Public Routes (Bisa diakses siapa saja) ---
 Route::post('/register', [AuthController::class, 'register']);
@@ -21,6 +22,15 @@ Route::middleware('auth:sanctum')->group(function () {
     // --- API Resource untuk ClassRoom ---
     // Ini otomatis membuat route index, store, show, update, destroy
     Route::apiResource('classrooms', \App\Http\Controllers\ClassRoomController::class);
+
+    // Route untuk membuat konten dalam kelas tertentu
+    Route::post('/classrooms/{classroom}/contents', [ClassContentController::class, 'store']);
+
+    // Route untuk melihat semua konten dalam kelas tertentu
+    Route::get('/classrooms/{classroom}/contents', [ClassContentController::class, 'index']);
+
+    // Route untuk melihat detail konten spesifik (tanpa perlu ID kelas)
+    Route::get('/contents/{id}', [ClassContentController::class, 'show']);
   });
 
   // Nanti route Kelas, Materi, Tugas akan ditaruh di sini...
