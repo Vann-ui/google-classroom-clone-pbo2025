088ef9fca23df707b43612ca387f031681589535
gabybaee <h1101241052@student.untan.ac.id>
2025-11-27 18:33:22 +0700

    Menambahkan Class Content

---
+++ app/Models/ClassContent.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ClassContent extends Model
+{
+    // Encapsulation: Melindungi atribut agar aman saat Mass Assignment
+    protected $fillable = [
+        'classroom_id',
+        'created_by',
+        'title',
+        'description',
+        'content_type', // 'material' atau 'assignment'
+    ];
+
+    /**
+     * Relasi ke Child: Material
+     * Menggunakan HasOne karena 1 Content punya 1 detail Material (jika tipenya material)
+     */
+    public function material(): HasOne
+    {
+        return $this->hasOne(Material::class, 'id');
+    }
+
+    /**
+     * Relasi ke Child: Assignment
+     */
+    public function assignment(): HasOne
+    {
+        return $this->hasOne(Assignment::class, 'id');
+    }
+
+    /**
+     * Relasi ke Pembuat (User/Teacher)
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+    
+    public function classroom(): BelongsTo
+    {
+        return $this->belongsTo(ClassRoom::class);
+    }
+}
